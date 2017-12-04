301fab0bfb198544af2b2989e1debe91845cd1ea
Jacek Kopecky <jacek@jacek.cz>
2017-12-04 22:05:03 +0000

    move from mysql to mysql2 with promises

--- examples/model-mysql.js
+++ examples/model-mysql.js
@@ -1,120 +1,93 @@
 'use strict';
 
 const fs = require('fs');
-const mysql = require('mysql');
+const util = require('util');
+const mysql = require('mysql2/promise');
+
+// promisify some filesystem functions
+fs.unlinkAsync = fs.unlinkAsync || util.promisify(fs.unlink);
+fs.renameAsync = fs.renameAsync || util.promisify(fs.rename);
 
 const config = require('./config');
 
-const sql = mysql.createConnection(config.mysql);
+const sqlPromise = mysql.createConnection(config.mysql);
 
-module.exports.listPictures = (title, sort) => {
-  return new Promise((resolve, reject) => {
-    // prepare query
-    let query = 'SELECT id, title, filename FROM picture';
-    if (title) {
-      query += ' WHERE title LIKE ' + sql.escape('%' + title + '%');
-    }
-    let order;
-    switch (sort) {
-      case 'asc':
-      case 'a2z': order = 'title ASC'; break;   // by title a-z
-      case 'desc':
-      case 'z2a': order = 'title DESC'; break;  // by title z-a
-      case 'random':
-      case 'rnd': order = 'rand()'; break;      // random order
-      case 'old': order = 'id ASC'; break;      // oldest-first (by submission time)
-      case 'new':                               // newest-first (by submission time)
-      default:    order = 'id DESC';
-    }
-    query += ' ORDER BY ' + order;
-    query += ' LIMIT 10';
+(async () => {
+  const sql = await sqlPromise;
+  // handle unexpected errors by just logging them
+  sql.on('error', (err) => {
+    console.error(err);
+    sql.end();
+  });
+})();
 
-    // now query the table and output the results
-    sql.query(query, (err, data) => {
-      if (err) {
-        reject(['failed to run the query', err]);
-        return;
-      }
+module.exports.listPictures = async (title, sort) => {
+  const sql = await sqlPromise;
 
-      const retval = [];
+  let query = 'SELECT id, title, filename FROM picture';
+  if (title) {
+    query += ' WHERE title LIKE ' + sql.escape('%' + title + '%');
+  }
+  let order;
+  switch (sort) {
+    case 'asc':
+    case 'a2z': order = 'title ASC'; break;   // by title a-z
+    case 'desc':
+    case 'z2a': order = 'title DESC'; break;  // by title z-a
+    case 'random':
+    case 'rnd': order = 'rand()'; break;      // random order
+    case 'old': order = 'id ASC'; break;      // oldest-first (by submission time)
+    case 'new':                               // newest-first (by submission time)
+    default:    order = 'id DESC';
+  }
+  query += ' ORDER BY ' + order;
+  query += ' LIMIT 10';
 
-      data.forEach((row) => {
-        retval.push({
-          id: row.id,
-          title: row.title,
-          file: config.webimg + row.filename,
-        });
-      });
+  // now query the table and output the results
+  const [rows] = await sql.query(query);
 
-      resolve(retval);
-    });
+  return rows.map((row) => {
+    return {
+      id: row.id,
+      title: row.title,
+      file: config.webimg + row.filename,
+    };
   });
 };
 
 
 const GONE = { status: 'gone' };
 
-module.exports.deletePicture = (id) => {
-  return new Promise((resolve, reject) => {
-    // get the filename from the table
-    sql.query(sql.format('SELECT filename FROM picture WHERE id = ?', [id]), (err, data) => {
-      if (err) {
-        reject(['failed to get filename for deletion', err]);
-        return;
-      }
+module.exports.deletePicture = async (id) => {
+  const sql = await sqlPromise;
 
-      if (data.length < 1) {
-        reject(GONE);
-        return;
-      }
+  // get the filename from the table
+  const [rows] = await sql.query(sql.format('SELECT filename FROM picture WHERE id = ?', [id]));
+  if (rows.length < 1) {
+    throw GONE;
+  }
 
-      const filename = config.localimg + data[0].filename;
+  const filename = config.localimg + rows[0].filename;
 
-      sql.query(sql.format('DELETE FROM picture WHERE id=?', [id]), (err) => {
-        if (err) {
-          reject(['failed sql delete', err]);
-          return;
-        }
-
-        fs.unlink(filename, (err) => {
-          if (err) {
-            reject(['failed fs delete of ' + filename, err]);
-            return;
-          }
-
-          resolve();
-        });
-      });
-    });
-  });
+  await sql.query(sql.format('DELETE FROM picture WHERE id=?', [id]));
+  await fs.unlinkAsync(filename);
 };
 
 
-module.exports.uploadPicture = (reqFile, title) => {
-  return new Promise((resolve, reject) => {
-    // move the file where we want it
-    const fileExt = reqFile.mimetype.split('/')[1] || 'png';
-    const newFilename = reqFile.filename + '.' + fileExt;
-    fs.rename(reqFile.path, config.localimg + newFilename, (err) => {
-      if (err) {
-        reject(['failed to move incoming file', err]);
-        return;
-      }
+module.exports.uploadPicture = async (reqFile, title) => {
+  const sql = await sqlPromise;
 
-      // now add the file to the DB
-      const dbRecord = {
-        filename: newFilename,
-        title,
-      };
+  // move the file where we want it
+  const fileExt = reqFile.mimetype.split('/')[1] || 'png';
+  const newFilename = reqFile.filename + '.' + fileExt;
+  await fs.renameAsync(reqFile.path, config.localimg + newFilename);
 
-      sql.query(sql.format('INSERT INTO picture SET ?', dbRecord), (err, result) => {
-        if (err) {
-          reject(['failed sql insert', err]);
-          return;
-        }
+  // now add the file to the DB
+  const dbRecord = {
+    filename: newFilename,
+    title,
+  };
 
-        resolve({ id: result.insertId, title: dbRecord.title, file: config.webimg + dbRecord.filename });
-      });
-    });
-  });
+  const [rows] = await sql.query(sql.format('INSERT INTO picture SET ?', dbRecord));
+  return { id: rows.insertId, title: dbRecord.title, file: config.webimg + dbRecord.filename };
 };
