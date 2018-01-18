056091f3cd9af2f0f4faf8207e090a10797ea2c7
Jacek Kopecky <jacek@jacek.cz>
2018-01-18 12:29:24 +0000

    :shirt: only indent

--- examples/webpages/pictures.js
+++ examples/webpages/pictures.js
@@ -1,149 +1,149 @@
 (function () {
-'use strict';
+  'use strict';
 
-const elSearchnow = document.querySelector('#searchnow');
-const elSearchform = document.querySelector('#searchform');
-const elSort = document.querySelector('#sort');
-const elSearch = document.querySelector('#search');
-const elMain = document.querySelector('main');
-const elUpload = document.querySelector('#upload');
+  const elSearchnow = document.querySelector('#searchnow');
+  const elSearchform = document.querySelector('#searchform');
+  const elSort = document.querySelector('#sort');
+  const elSearch = document.querySelector('#search');
+  const elMain = document.querySelector('main');
+  const elUpload = document.querySelector('#upload');
 
-window.addEventListener('load', init);
+  window.addEventListener('load', init);
 
-function init() {
-  elSearchnow.addEventListener('click', loadPicturesWithSearch);
-  elSearchform.addEventListener('submit', loadPicturesWithSearch);
-  elSort.addEventListener('change', loadPictures);
+  function init() {
+    elSearchnow.addEventListener('click', loadPicturesWithSearch);
+    elSearchform.addEventListener('submit', loadPicturesWithSearch);
+    elSort.addEventListener('change', loadPictures);
 
-  document.body.addEventListener('dragover', dragOver);
-  document.body.addEventListener('dragenter', dragEnter);
-  document.body.addEventListener('dragleave', dragLeave);
-  document.body.addEventListener('drop', drop);
+    document.body.addEventListener('dragover', dragOver);
+    document.body.addEventListener('dragenter', dragEnter);
+    document.body.addEventListener('dragleave', dragLeave);
+    document.body.addEventListener('drop', drop);
 
-  loadPictures();
-}
-
-function loadPicturesWithSearch(e) {
-  e.preventDefault();
-  loadPictures();
-}
-
-async function loadPictures() {
-  try {
-    elMain.classList.add('loading');
-    let url = '/api/pictures';
-    url += '?order=' + elSort.selectedOptions[0].value;
-    if (elSearch.value) url += '&title=' + encodeURIComponent(elSearch.value);
-
-    const response = await fetch(url);
-    if (!response.ok) throw response;
-    elMain.classList.remove('loading');
-    putPicturesInPage(await response.json());
-  } catch (e) {
-    console.error('error getting pictures', e);
-    elMain.classList.remove('loading');
-    elMain.classList.add('error');
-  }
-}
-
-function putPicturesInPage(pics) {
-  // clear out old pictures
-  for (const old of elMain.querySelectorAll('section.picture')) {
-    old.remove();
-  }
-
-  // install new ones in the order they came
-  pics.forEach((pic) => {
-    const container = document.createElement('section');
-    container.classList.add('picture');
-    elMain.appendChild(container);
-
-    const a = document.createElement('a');
-    a.classList.add('img');
-    a.href = pic.file;
-    container.appendChild(a);
-
-    let el = document.createElement('img');
-    el.src = pic.file;
-    el.alt = pic.title;
-    a.appendChild(el);
-
-    el = document.createElement('p');
-    el.classList.add('title');
-    el.textContent = pic.title;
-    container.appendChild(el);
-
-    el = document.createElement('div');
-    el.classList.add('delete');
-    el.textContent = 'X';
-    el.dataset.id = pic.id;
-    el.onclick = requestDelete;
-    container.appendChild(el);
-  });
-}
-
-async function requestDelete(e) {
-  if (e.target.dataset.id && window.confirm('Realy delete this image?')) {
-    await fetch('/api/pictures/' + e.target.dataset.id, { method: 'DELETE' });
     loadPictures();
   }
-}
 
-
-function isDragAcceptable(e) {
-  return e.dataTransfer.items.length > 0 &&
-    e.dataTransfer.items[0].type.startsWith('image/');
-}
-
-function dragOver(e) {
-  e.preventDefault();
-  e.dataTransfer.dropEffect = 'copy';
-  e.dataTransfer.effectAllowed = 'copy';
-
-  if (elUpload.contains(e.target) && !isDragAcceptable(e)) {
-    elUpload.classList.add('highlight-error');
+  function loadPicturesWithSearch(e) {
+    e.preventDefault();
+    loadPictures();
   }
-}
 
-let dragDepth = 0;
+  async function loadPictures() {
+    try {
+      elMain.classList.add('loading');
+      let url = '/api/pictures';
+      url += '?order=' + elSort.selectedOptions[0].value;
+      if (elSearch.value) url += '&title=' + encodeURIComponent(elSearch.value);
 
-function dragEnter(e) {
-  elUpload.classList.add('highlight-for-drop');
-  dragDepth += 1;
-
-  if (elUpload.contains(e.target) && !isDragAcceptable(e)) {
-    elUpload.classList.add('highlight-error');
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      elMain.classList.remove('loading');
+      putPicturesInPage(await response.json());
+    } catch (e) {
+      console.error('error getting pictures', e);
+      elMain.classList.remove('loading');
+      elMain.classList.add('error');
+    }
   }
-}
 
-function dragLeave(e) {
-  dragDepth -= 1;
-  if (dragDepth === 0) {
+  function putPicturesInPage(pics) {
+    // clear out old pictures
+    for (const old of elMain.querySelectorAll('section.picture')) {
+      old.remove();
+    }
+
+    // install new ones in the order they came
+    pics.forEach((pic) => {
+      const container = document.createElement('section');
+      container.classList.add('picture');
+      elMain.appendChild(container);
+
+      const a = document.createElement('a');
+      a.classList.add('img');
+      a.href = pic.file;
+      container.appendChild(a);
+
+      let el = document.createElement('img');
+      el.src = pic.file;
+      el.alt = pic.title;
+      a.appendChild(el);
+
+      el = document.createElement('p');
+      el.classList.add('title');
+      el.textContent = pic.title;
+      container.appendChild(el);
+
+      el = document.createElement('div');
+      el.classList.add('delete');
+      el.textContent = 'X';
+      el.dataset.id = pic.id;
+      el.onclick = requestDelete;
+      container.appendChild(el);
+    });
+  }
+
+  async function requestDelete(e) {
+    if (e.target.dataset.id && window.confirm('Realy delete this image?')) {
+      await fetch('/api/pictures/' + e.target.dataset.id, { method: 'DELETE' });
+      loadPictures();
+    }
+  }
+
+
+  function isDragAcceptable(e) {
+    return e.dataTransfer.items.length > 0 &&
+      e.dataTransfer.items[0].type.startsWith('image/');
+  }
+
+  function dragOver(e) {
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'copy';
+    e.dataTransfer.effectAllowed = 'copy';
+
+    if (elUpload.contains(e.target) && !isDragAcceptable(e)) {
+      elUpload.classList.add('highlight-error');
+    }
+  }
+
+  let dragDepth = 0;
+
+  function dragEnter(e) {
+    elUpload.classList.add('highlight-for-drop');
+    dragDepth += 1;
+
+    if (elUpload.contains(e.target) && !isDragAcceptable(e)) {
+      elUpload.classList.add('highlight-error');
+    }
+  }
+
+  function dragLeave(e) {
+    dragDepth -= 1;
+    if (dragDepth === 0) {
+      elUpload.classList.remove('highlight-for-drop');
+    }
+
+    if (e.target === elUpload) {
+      elUpload.classList.remove('highlight-error');
+    }
+  }
+
+  function drop(e) {
+    dragDepth = 0;
     elUpload.classList.remove('highlight-for-drop');
-  }
-
-  if (e.target === elUpload) {
     elUpload.classList.remove('highlight-error');
+    e.preventDefault();
+
+    if (!elUpload.contains(e.target)) return; // dropped outside of the upload zone
+
+    if (!isDragAcceptable(e)) return; // don't allow the drop
+
+    // todo handle the dropped file, upload it
+    console.log(
+      e.dataTransfer.types,
+      e.dataTransfer.files,
+      e.dataTransfer.items,
+      e.dataTransfer.items[0],
+      e.dataTransfer.items[0].getAsFile(),
+    );
   }
-}
-
-function drop(e) {
-  dragDepth = 0;
-  elUpload.classList.remove('highlight-for-drop');
-  elUpload.classList.remove('highlight-error');
-  e.preventDefault();
-
-  if (!elUpload.contains(e.target)) return; // dropped outside of the upload zone
-
-  if (!isDragAcceptable(e)) return; // don't allow the drop
-
-  // todo handle the dropped file, upload it
-  console.log(
-    e.dataTransfer.types,
-    e.dataTransfer.files,
-    e.dataTransfer.items,
-    e.dataTransfer.items[0],
-    e.dataTransfer.items[0].getAsFile(),
-  );
-}
 }());
