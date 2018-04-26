c0e44b47135e729cfa54e5cb0949bf8918a612de
Jacek Kopecky <jacek@jacek.cz>
2018-04-26 10:18:08 +0100

    upgrade to qunit 2.6
    a few polishings
    coverage check

--- examples/test.js
+++ examples/test.js
@@ -1,4 +1,4 @@
-/* global require ok test deepEqual equal */
+/* global QUnit */
 
 'use strict';
 
@@ -27,52 +27,52 @@ function arrayInOrder(arr, order) {
 }
 
 // These tests depend on the initial data in `model-inmemory.js`
-test(
+QUnit.test(
   '`listPictures()`.',
-  () => {
-    deepEqual(
+  (assert) => {
+    assert.deepEqual(
       db.listPictures(null, 'asc'),
       arrayInOrder(initialData, [1, 4, 3, 2]),
       'It works in ascending order.',
     );
 
-    deepEqual(
+    assert.deepEqual(
       db.listPictures(null, 'a2z'),
       arrayInOrder(initialData, [1, 4, 3, 2]),
       'It works when we use the alias `a2z`.',
     );
 
-    deepEqual(
+    assert.deepEqual(
       db.listPictures(null, 'desc'),
       arrayInOrder(initialData, [2, 3, 4, 1]),
       'It works in descending order.',
     );
 
-    deepEqual(
+    assert.deepEqual(
       db.listPictures(null),
       arrayInOrder(initialData, [4, 3, 2, 1]),
       'The default case works.',
     );
 
-    deepEqual(
+    assert.deepEqual(
       db.listPictures(null, 'old'),
       arrayInOrder(initialData, [1, 2, 3, 4]),
       'It works in order from oldest to newest.',
     );
 
-    equal(
+    assert.equal(
       db.listPictures(null, 'rnd').length,
       4,
       'Returns the good number of values when random.',
     );
 
-    deepEqual(
+    assert.deepEqual(
       arrayInOrder(db.listPictures(db.title, 'rnd'), [1, 2, 3, 4]),
       arrayInOrder(initialData, [1, 2, 3, 4]),
       'It works when we try a random',
     );
 
-    deepEqual(
+    assert.deepEqual(
       db.listPictures(null, 'new'),
       arrayInOrder(initialData, [4, 3, 2, 1]),
       'It works in order from newest to oldest.',
@@ -85,38 +85,30 @@ test(
 * and another one to test an exception for the `deletePicture` function.
 * The last upload here must send an exception because we don't create the file for this test.
 */
-test(
+QUnit.test(
   '`uploadPicture()`.',
-  async () => {
+  async (assert) => {
     const picture = {
       mimetype: 'image/png',
       filename: 'toDelete',
       path: pathImage + '/webpages/img/toDelete.png',
     };
 
-    try {
-      const file = fs.openSync(picture.path, 'w');
-      fs.close(file);
-    } catch (e) {
-      console.log(e);
-    }
+    const file = fs.openSync(picture.path, 'w');
+    fs.closeSync(file);
 
-    try {
-      const exists = fs.existsSync(pathImage + '/webpages/img/toDelete.png');
-      ok(exists, 'The file `toDelete.png` is in the good place.');
-    } catch (e) {
-      ok(false, 'The file `toDelete.png` is not at the good place.');
-    }
+    const exists = fs.existsSync(pathImage + '/webpages/img/toDelete.png');
+    assert.ok(exists, 'The file `toDelete.png` is in the good place.');
 
     initialData.push({ id: 5, title: 'testTitle', file: '/img/toDelete.png' });
 
-    deepEqual(
+    assert.deepEqual(
       await db.uploadPicture(picture, 'testTitle', 'testAuthor'),
       { id: 5, title: 'testTitle', file: '/img/toDelete.png' },
       'The picture was correctly uploaded.',
     );
 
-    deepEqual(
+    assert.deepEqual(
       db.listPictures(null, 'new'),
       arrayInOrder(initialData, [5, 4, 3, 2, 1]),
       'The picture is in the memory.',
@@ -127,9 +119,12 @@ test(
 
     try {
       await db.uploadPicture(picture, 'testTitle', 'testAuthor');
-      ok(false, 'uploadPicture should have thrown an exception.');
+      assert.ok(false, 'uploadPicture should have thrown an exception.');
     } catch (e) {
-      ok(e[0] === 'failed to move incoming file', 'It threw the right exception : `failed to move incoming file`.');
+      assert.ok(
+        e[0] === 'failed to move incoming file',
+        'It threw the right exception : `failed to move incoming file`.',
+      );
     }
   },
 );
@@ -137,19 +132,15 @@ test(
 initialData.splice(5);
 
 // Here, we will use both images created before to try the `deletePicture` function.
-test(
+QUnit.test(
   '`deletePicture()`.',
-  async () => {
+  async (assert) => {
     await db.deletePicture(5);
 
-    const notExists = fs.existsSync(pathImage + '/webpages/img/toDelete.png');
-    if (notExists === false) {
-      ok(true, 'The file `toDelete.png` has been deleted.');
-    } else {
-      ok(false, 'We can still find the picture `toDelete.png`.');
-    }
+    const exists = fs.existsSync(pathImage + '/webpages/img/toDelete.png');
+    assert.ok(!exists, 'The file `toDelete.png` has been deleted.');
 
-    deepEqual(
+    assert.deepEqual(
       db.listPictures(null, 'new'),
       arrayInOrder(initialData, [4, 3, 2, 1]),
       'The picture was deleted.',
@@ -157,9 +148,9 @@ test(
 
     try {
       await db.deletePicture(6);
-      ok(false, 'deletePicture should have thrown an exception.');
+      assert.ok(false, 'deletePicture should have thrown an exception.');
     } catch (e) {
-      deepEqual(e, GONE, 'It threw the right exception GONE.');
+      assert.deepEqual(e, GONE, 'It threw the right exception GONE.');
     }
 
     const wrongPicture = {
@@ -168,20 +159,16 @@ test(
       path: pathImage + '/webpages/img/toDelete2.png',
     };
 
-    try {
-      const file = fs.openSync(wrongPicture.path, 'w');
-      fs.close(file);
-      await db.uploadPicture(wrongPicture, 'testThrowException', 'students');
-      fs.unlinkSync(wrongPicture.path);
-    } catch (e) {
-      console.log(e);
-    }
+    const file = fs.openSync(wrongPicture.path, 'w');
+    fs.closeSync(file);
+    await db.uploadPicture(wrongPicture, 'testThrowException', 'students');
+    fs.unlinkSync(wrongPicture.path);
 
     try {
       await db.deletePicture(6);
-      ok(false, 'deletePicture should have thrown an exception.');
+      assert.ok(false, 'deletePicture should have thrown an exception.');
     } catch (e) {
-      deepEqual(e[0], 'failed fs delete of ' + wrongPicture.path, 'It threw the right exception FS.');
+      assert.deepEqual(e[0], 'failed fs delete of ' + wrongPicture.path, 'It threw the right exception FS.');
     }
   },
 );
